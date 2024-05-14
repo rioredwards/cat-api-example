a01233556cdcec3db421b58a33a375d4b1f830b5
Rio Edwards <rioredwards@gmail.com>
2024-05-14 11:38:17 -0700

    Revert "Fetch random cat using button"
    This reverts commit 04e92d1d746d09f3acf9914f99169c0eea6799ff.

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
 const catContainer = document.getElementById("cat-container");
-const randomButton = document.getElementById("fetch-random");
 
-randomButton.addEventListener("click", () => {
-  const url =
-    "https://api.thecatapi.com/v1/images/search?has_breeds=1&api_key=live_YNdBDkb6hJuKn2sTeEUeI2J2p14xqL77loXkCwJJVr284EgKEM1nSrVDrdrpCZKb";
+const url =
+  "https://api.thecatapi.com/v1/images/search?has_breeds=1&api_key=live_YNdBDkb6hJuKn2sTeEUeI2J2p14xqL77loXkCwJJVr284EgKEM1nSrVDrdrpCZKb";
 
-  catContainer.innerHTML = "";
+fetch(url)
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("Something went wrong!");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data[0].breeds[0]);
 
-  fetch(url)
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error("Something went wrong!");
-      }
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data[0].breeds[0]);
+    const catBreedName = document.createElement("h2");
+    catBreedName.innerText = data[0].breeds[0].name;
 
-      const catBreedName = document.createElement("h2");
-      catBreedName.innerText = data[0].breeds[0].name;
+    const catBreedDescription = document.createElement("p");
+    catBreedDescription.innerText = data[0].breeds[0].description;
 
-      const catBreedDescription = document.createElement("p");
-      catBreedDescription.innerText = data[0].breeds[0].description;
+    const catImg = document.createElement("img");
+    catImg.src = data[0].url;
 
-      const catImg = document.createElement("img");
-      catImg.src = data[0].url;
-
-      catContainer.appendChild(catBreedName);
-      catContainer.appendChild(catBreedDescription);
-      catContainer.appendChild(catImg);
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
-});
+    catContainer.appendChild(catBreedName);
+    catContainer.appendChild(catBreedDescription);
+    catContainer.appendChild(catImg);
+  })
+  .catch((error) => {
+    console.warn(error);
+  });
