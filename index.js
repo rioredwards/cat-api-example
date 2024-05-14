b6270c7176ca3084eabd2b346066b76ef04216ff
Rio Edwards <rioredwards@gmail.com>
2024-05-13 20:32:25 -0700

    Fetching

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("hello, world!");
+
+const url =
+  "https://api.thecatapi.com/v1/images/search?has_breeds=1&api_key=live_YNdBDkb6hJuKn2sTeEUeI2J2p14xqL77loXkCwJJVr284EgKEM1nSrVDrdrpCZKb";
+
+fetch(url)
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("Something went wrong!");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.warn(error);
+  });
